27dd9b55d9dbfca13ab3c31e1af0a4e4fd752b71
farandal <farandal@gmail.com>
2013-08-15 02:09:39 -0400

    fix some bugs, instructions added at frontend, small css fixes to fit resolution, added feature to select diferent feeds, feeds are now loading dinamically from external file

--- public/js/accedo.js
+++ public/js/accedo.js
@@ -1,55 +1,101 @@
 // JavaScript Document
 var Accedo = {
     init: function () {
-        this.debug = false;
-        if (this.debug) console.log("init");
-        this.feeds = ["http://rss.cnn.com/services/podcasting/incaseyoumissed/rss.xml", "http://rss.cnn.com/services/podcasting/ac360/rss.xml"];
+        this.debugging = true;
+        this.debug = function(msg) {
+			if(this.debugging) console.log(msg);
+	    }
+        this.feeds = []; //Feeds loaded dinamically from js/feeds.json
         this.proxy = "google";
         this.data = null;
         this.cur_index = 0;
         this.cur_item = null;
+		this.feed_index = 0;
+		this.cur_feed = null;
         this.loadFeeds();
         this.maxItemsInNav = 4;
         this.player = "jwplayer";
         this.playerobj = null;
         this.Player.init(this);
+		this.state = "items";
+		this.keyboardListener(); 
     },
+	
     loadFeeds: function () {
-        //Load Feeds function is intended to load all feeds in the feeds array, for now, only using the first one
-        /*The application must read the RSS file for one of ... */
-        if (this.debug) console.log("loadFeeds");
-        this.loadRSS(this.feeds[0]);
+        //Load Feeds function is intended to load all feeds in the feeds array, for now, only using the first one on load
+		
+        this.debug("loadFeeds");
+		
+		
+		var $t = this;
+		
+		$.getJSON('js/feeds.json', function(data) {
+		  var json_feeds = [];
+		  $.each(data, function(key, val) {
+			json_feeds.push({'name':key,'url':val});
+		  });
+		  
+		  $t.debug(json_feeds);
+		  $t.feeds = json_feeds;
+		  $t.countFeeds($t.feed_index);
+		  $t.previewFeed($t.feed_index);
+		  $t.loadRSS($t.feed_index);
+		
+		});
+		
     },
-    loadRSS: function (url) {
-        if (this.debug) console.log("loadRSS");
+	changeFeed: function() {
+		 
+		 this.loadRSS(this.feed_index);
+		
+	},
+	previewFeed: function(idx) {
+		this.debug(idx);
+		this.debug(this.feeds[idx]);
+		var feed_name = this.feeds[idx].name;
+		$("#feed_name div#text").html(feed_name);
+		
+		
+	},
+    loadRSS: function (idx) {
+		this.cleanNavBar();
+		url = this.feeds[idx].url;
+        this.debug("loadRSS");
         switch (this.proxy) {
         case "google":
-            if (this.debug) console.log("using google");
+            this.debug("using google");
             proxy_url = document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url);
-            $.ajax({
+         
+			$.ajax({
                 context: this,
                 url: proxy_url,
                 dataType: 'json',
                 success: function (data) {
-                    this.displayFeed(data.responseData.feed);
+                   this.displayFeed(data.responseData.feed);
                 }
             });
             break;
         case "custom":
             //Not implemented, using google as proxy
-            if (this.debug) console.log("using custom");
+            this.debug("using custom");
             this.displayFeed([]);
             break;
         }
     },
     displayFeed: function (data) {
         this.data = data;
-        if (this.debug) console.log("displayFeed");
-        this.displayItem(this.cur_index); //Dispay the first item when the feed is loaded
+        this.debug("displayFeed");
         this.populateNavBar();
-        var $c = this;
-        $(document).keydown(function (e) {
-            if (e.keyCode == 37) {
+		this.displayItem(0); //Dispay the first item when the feed is loaded
+       
+       
+    },
+	keyboardListener: function() {
+	
+		var $c = this; //This is not elegant, should improve this, I should not send the context as parameter, just a bug fix.
+		$(document).keydown(function (e) {
+            
+			if (e.keyCode == 37) {
                 $c.Controls.left($c);
                 return false;
             }
@@ -69,27 +115,35 @@ var Accedo = {
                 $c.Controls.enter($c);
                 return false;
             }
+			
+			
         });
-    },
+		
+	},
     displayItem: function (index) {
         //current item
-        if (this.debug) console.log("displayItem " + index);
+        this.debug("displayItem " + index);
         this.cur_item = this.data.entries[index];
         $("#tit_ctrl").html(this.cur_item.title);
         $("#desc_ctrl").html(this.cur_item.contentSnippet);
         $("#video_desc_ctrl").html(this.cur_item.content);
-        if (this.debug) console.log(this.cur_item);
+        this.debug(this.cur_item);
         this.Player.video(this);
     },
     countItems: function () {
-        if (this.debug) console.log("countItems");
-        if (this.debug) console.log($(this.data.entries).size());
+        this.debug("countItems "+$(this.data.entries).size());
+       
         return $(this.data.entries).size();
+    },
+	countFeeds: function () {
+        this.debug("countFeeds");
+        this.debug($(this.feeds).size());
+        return $(this.feeds).size();
     },
     populateNavBar: function () {
         //recorrer this.data
-        if (this.debug) console.log("populate nav bar");
-        if (this.debug) console.log(this.data);
+        this.debug("populate nav bar");
+        this.debug(this.data);
         var $i = 0;
         $.each(this.data.entries, function (key, value) {
             $class = "";
@@ -98,6 +152,11 @@ var Accedo = {
             $(".itemlist").append("<li class='item " + $class + "' id='item" + $i + "'><span class='title'>" + value.title + "</span><span class='date'>" + value.publishedDate + "</span></li>");
             $i++;
         });
+    },
+	cleanNavBar: function () {
+        //recorrer this.data
+		//this.cur_index = 0;
+        $(".itemlist li").remove();
     },
     selectItem: function (i)  {
         $(".itemlist .item").removeClass("selected");
@@ -110,21 +169,40 @@ var Accedo = {
         $(".itemlist").css({
             "top": -t + "px"
         });
-        if (this.debug) console.log(t);
+        this.debug(t);
     },
     Controls: {
         up: function (ctx) {
-            if (ctx.cur_index > 0) ctx.cur_index = ctx.cur_index - 1;
+            ctx.state = "items";
+			ctx.countItems();
+			if (ctx.cur_index > 0) ctx.cur_index -= 1;
             ctx.selectItem(ctx.cur_index);
+			ctx.debug("cur_index = "+ctx.cur_index);
         },
         down: function (ctx) {
-            if (ctx.cur_index < ctx.countItems() - 1) ctx.cur_index = ctx.cur_index + 1;
+			ctx.state = "items";
+            if (ctx.cur_index < ctx.countItems() - 1) ctx.cur_index += 1;
             ctx.selectItem(ctx.cur_index);
+			ctx.debug("cur_index = "+ctx.cur_index);
         },
-        left: function (ctx) {},
-        right: function (ctx) {},
+        left: function (ctx) {
+			 ctx.state = "feeds";
+			 if (ctx.feed_index > 0) ctx.feed_index -= 1;
+             ctx.previewFeed(ctx.feed_index);
+			 ctx.debug("feed_index = "+ctx.feed_index);
+			
+		},
+        right: function (ctx) {
+			 ctx.state = "feeds";
+			 if (ctx.feed_index < ctx.countFeeds() - 1) ctx.feed_index += 1;
+             ctx.previewFeed(ctx.feed_index);
+			 ctx.debug("feed_index = "+ctx.feed_index);
+			
+		},
         enter: function (ctx) {
-            ctx.displayItem(ctx.cur_index);
+			if(ctx.state == "items") ctx.displayItem(ctx.cur_index);
+			if(ctx.state == "feeds") ctx.changeFeed(ctx.cur_feed);
+			
         },
     },
     Player: {
